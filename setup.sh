ff7204715525a93019c78fecf6b90a3ed15be5ff
SunilKumar005 <suni.71772118147@gct.ac.in>
2024-11-04 15:15:36 +0000

    Modified setup.sh

--- setup.sh
+++ setup.sh
@@ -13,7 +13,7 @@ EOF
 
 
 # Install necessary Python libraries
-pip install fastapi uvicorn sqlalchemy psycopg2-binary pydantic pydoop
+pip install fastapi uvicorn sqlalchemy psycopg2-binary pydantic pydoop pyhive thrift thrift-sasl
 
 # Optionally install other libraries you might need for your project
 # pip install <other-libraries>
